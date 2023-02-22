0b99019da3abed244c1da02f09a03dfe1fe4efff
AxelBaranowski3il <baranowa@3il.fr>
2023-02-22 18:54:32 +0100

    fix tests

--- fr/rodez3il/a2022/mrmatt/solveur/Noeud.java
+++ fr/rodez3il/a2022/mrmatt/solveur/Noeud.java
@@ -1,20 +1,124 @@
 package fr.rodez3il.a2022.mrmatt.solveur;
 
-import fr.rodez3il.a2022.mrmatt.solveur.structures.*;
-import fr.rodez3il.a2022.mrmatt.sources.*;
+import fr.rodez3il.a2022.mrmatt.solveur.structures.DictionnaireChaine;
+import fr.rodez3il.a2022.mrmatt.sources.Commande;
+import fr.rodez3il.a2022.mrmatt.sources.Niveau;
+import fr.rodez3il.a2022.mrmatt.solveur.structures.ListeChainee;
 
 public class Noeud {
-    private DictionnaireChaine dicoChaine;
-    private Niveau etatNiveau;
-    private Object[] fils;
-    private Commande[] commandesASuivre;
+    // Référence vers le dictionnaire des configurations connues
+    private DictionnaireChaine<String, Noeud> dict;
 
-    public Noeud(DictionnaireChaine dicoChaine, Niveau etatNiveau, Object[] fils, Commande[] commandesASuivre){
-        this.dicoChaine = dicoChaine;
-        this.etatNiveau = etatNiveau;
+    // Référence vers le niveau actuel
+    private Niveau niveau;
+
+    // indique si le noeud est déjà visité
+    private boolean visite = false;
+
+    private Noeud[] fils;
+
+    // Enum des commandes possibles pour atteindre les configurations
+    private Commande commande;
+
+    private String chaineCommande;
+
+    public Noeud(DictionnaireChaine<String, Noeud> dict, Niveau niveau, Noeud[] fils, String chaine, boolean visite) {
+        this.dict = dict;
+        this.niveau = niveau;
+        this.visite = visite;
         this.fils = fils;
-        this.commandesASuivre = commandesASuivre;
+        this.chaineCommande = chaine;
     }
 
+    /**
+     * Accesseur de l'attribut visite
+     *
+     * @return visite
+     */
+    public boolean getVisite() {
+        return this.visite;
+    }
 
+    /**
+     * mutateur de l'attribut visite
+     *
+     * @return visite
+     */
+    public void setVisite(boolean newVisite) {
+        this.visite = newVisite;
+    }
+
+    /**
+     * Accesseur de l'attribut haut
+     *
+     * @return visite
+     */
+    public Niveau getNiveau() {
+        return this.niveau;
+    }
+
+    /**
+     * Accesseur de l'attribut Fils
+     *
+     * @return visite
+     */
+    public Noeud[] getFils() {
+        return this.fils;
+    }
+
+    /**
+     * Accesseur de l'attribut chaineCommande
+     *
+     * @return visite
+     */
+    public String getChaineCommande() {
+        return this.chaineCommande;
+    }
+
+    /**
+     * Redéfinition de la méthode equals
+     *
+     * @param otherObj l'objet à comparer avec le noeud
+     * @return true si égal sinon false
+     */
+    public boolean equals(Object otherObj) {
+        if (otherObj instanceof Noeud) {
+            Noeud other = (Noeud) otherObj;
+            return this.niveau.equals(other.niveau);
+        }
+        return false;
+    }
+
+    /**
+     * Calcule les fils du Noeud pour toutes les directions
+     * et déplacements possibles. Si oui Vérifie si le niveau est gagant
+     * Ajoute au dictionnaire connue la chaine et ajoute un noeud au fils
+     */
+    public void calculerFils() {
+        this.visite = true;
+        for (Commande comm : Commande.values()) {
+            if (niveau.deplacementPossible(comm)) {
+
+                Niveau niveauTemp = niveau.copier();
+                chaineCommande += comm.toString() + "\n";
+
+                niveauTemp.deplacer(comm);
+
+                if (niveau.estGagnant()) {
+                    return;
+                }
+
+                Noeud newFils;
+                String valeurChaine = niveauTemp.valeurChaine();
+
+                if (dict.contient(valeurChaine)) {
+                    newFils = dict.valeur(valeurChaine);
+                } else {
+                    newFils = new Noeud(dict, niveauTemp, new Noeud[4], chaineCommande, false);
+                    dict.inserer(valeurChaine, newFils);
+                }
+                fils[comm.ordinal()] = newFils;
+            }
+        }
+    }
 }
--- fr/rodez3il/a2022/mrmatt/solveur/Solveur.java
+++ fr/rodez3il/a2022/mrmatt/solveur/Solveur.java
@@ -1,13 +1,51 @@
 package fr.rodez3il.a2022.mrmatt.solveur;
 
+import fr.rodez3il.a2022.mrmatt.solveur.structures.DictionnaireChaine;
 import fr.rodez3il.a2022.mrmatt.sources.Niveau;
+
+import java.util.LinkedList;
 
 public class Solveur {
 
 	public static String trouverSolution(Niveau niveau) {
+		DictionnaireChaine<String, Noeud> dict = new DictionnaireChaine<>();
+		// Initialisation du Noeud sans commande
+		Noeud initial = new Noeud(dict, niveau, new Noeud[4], "", false);
+		dict.inserer(niveau.valeurChaine(), initial);
+
+		// Déclaration des deux listes traités et à traiter
+		LinkedList<Noeud> traites = new LinkedList<>();
+		LinkedList<Noeud> aTraiter = new LinkedList<>();
+
+		// On ajoute le premier Noeud initial à la liste à traiter
+		aTraiter.add(initial);
+
+		// chaine qui écrira la solution
+		StringBuilder sb = new StringBuilder();
+		// Tant que la liste aTraiter n'est pas vide
+		while (!aTraiter.isEmpty()) {
+
+			// le noeud courant est égal au premier élément de la liste qu'on supprime juste après
+			Noeud courant = aTraiter.removeFirst();
+
+			// On ajoute le noeud qui doit être traité
+			traites.add(courant);
+
+			// On calcule le fils
+			courant.calculerFils();
+			for (Noeud fils : courant.getFils()) {
+				if (fils != null && !fils.getVisite()) {
+					if (fils.getNiveau().estGagnant()) {
+						return fils.getChaineCommande();
+					}
+					//fils.setVisite(true);
+					aTraiter.add(fils);
+				}
+			}
+		}
 		return null;
 	}
-	
+
 	public static void main(String[] args) {
 		String solution = trouverSolution(new Niveau("niveaux/1rocher.txt"));
 		if (solution == null) {
